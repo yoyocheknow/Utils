61068e72c9a3476ed57a82e2fb0c573fab85fca4
yoyocheknow <911dangerous@gmail.com>
2017-11-16 10:46:47 +0800

    add BeanToMap.java

---
+++ BeanToMap.java
@@ -0,0 +1,36 @@
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2017/10/17.
+ */
+public class BeanToMap {
+    public static Map<String, Object> transBean2Map(Object obj) {
+        if (obj == null) {
+            return null;
+        }
+        Map<String, Object> map = new HashMap<String, Object>();
+        try {
+            BeanInfo beanInfo = Introspector.getBeanInfo(obj.getClass());
+            PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
+            for (PropertyDescriptor property : propertyDescriptors) {
+                String key = property.getName();
+                // 过滤class属性
+                if (!key.equals("class")) {
+                    // 得到property对应的getter方法
+                    Method getter = property.getReadMethod();
+                    Object value = getter.invoke(obj);
+                    map.put(key, value);
+                }
+            }
+        } catch (Throwable th) {
+            com.nyonline.nyutil.BizLogger.error(th,"transBean2Map Error {}");
+        }
+        BizLogger.info("map",map);
+        return map;
+    }
+}
